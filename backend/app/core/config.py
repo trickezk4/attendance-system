52e9e0724a234cdc4d2b508822cc1bf22439bc86
trickez <dacvu44@gmail.com>
2026-05-24 21:43:44 +0700

    setting for aws env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,19 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
-OVER_TIME_MONTHLY_LIMIT_MINS = int(os.getenv("OVERTIME_MONTHLY_LIMIT_MINS", 2400))
-OVER_TIME_YEARLY_LIMIT_MINS = int(os.getenv("OVERTIME_YEARLY_LIMIT_MINS", 12000))
-OVERTIME_DAILY_LIMIT_MINS=int(os.getenv("OVERTIME_DAILY_LIMIT_MINS", 240))
+class Settings:
+    DB_HOST: str = os.getenv("DB_HOST", "localhost") # Nếu không tìm thấy, dùng default
+    DB_USER: str = os.getenv("DB_USER", "root")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "")
+    DB_NAME: str = os.getenv("DB_NAME", "attendance_db")
+    DB_PORT: str = os.getenv("DB_PORT", "3306")
+
+settings = Settings()
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# OVER_TIME_MONTHLY_LIMIT_MINS = int(os.getenv("OVERTIME_MONTHLY_LIMIT_MINS", 2400))
+# OVER_TIME_YEARLY_LIMIT_MINS = int(os.getenv("OVERTIME_YEARLY_LIMIT_MINS", 12000))
+# OVERTIME_DAILY_LIMIT_MINS=int(os.getenv("OVERTIME_DAILY_LIMIT_MINS", 240))

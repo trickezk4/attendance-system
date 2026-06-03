c814511609146e130cdf9b588a85b30504cdea1b
trickez <dacvu44@gmail.com>
2026-06-04 06:01:02 +0700

    fix: 404 not found after mixed content

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,8 @@ app = FastAPI(
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "http://98.89.168.237"
+    "http://98.89.168.237",
+    "https://attendance-pj.duckdns.org" 
 ]
 
 app.add_middleware(
